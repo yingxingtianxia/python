5f4c6454e4078ae42ea31497b162ea6471c72a4c
yingxingtianxia <903511044@qq.com>
2019-01-07 20:17:39 +0800

    add new files

---
+++ PycharmProjects/liveshow/func1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def chengfabiao(n=9):
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            print("%s X %s = %s\t" % (j,i,i*j), end="")
+        print()
+
+if __name__ == '__main__':
+    chengfabiao()
