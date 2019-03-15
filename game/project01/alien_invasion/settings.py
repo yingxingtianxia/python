614a9a4aa3f16154f21e813d9160f6023e8dc60c
yingxingtianxia <903511044@qq.com>
2019-03-15 14:05:22 +0800

    add new files

---
+++ game/project01/alien_invasion/settings.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#存储外星人入侵项目所有的类
+
+class Settings():
+    #初始化游戏设置
+    def __init__(self):
+        #屏幕设置
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
