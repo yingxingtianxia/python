67617ad357f4913dec64c21a9d726117395f2241
yingxingtianxia <903511044@qq.com>
2019-03-18 14:27:54 +0800

    add new files

--- game/alien_invasion/game_functions.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import sys
-import pygame
-
-#检测键盘和鼠标事件
-def check_events():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
